ded0d8498d9706d420510cb901f30b185795c25d
Danya <theincredibledanzel@yandex.ru>
2022-06-11 08:30:34 +0400

    Task_2_complete

--- Task_2_min_max/Program.cs
+++ Task_2_min_max/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to the program!");
+Console.Write("Input first integer number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("Max = " + number1 + ", ");
+    Console.Write("Min = " + number2);
+}
+else
+{
+    Console.Write("Max = " + number2 + ", ");
+    Console.Write("Min = " + number1);
+}
+Console.WriteLine("");
+Console.Write("Thank you for participating! Program complete^^");
