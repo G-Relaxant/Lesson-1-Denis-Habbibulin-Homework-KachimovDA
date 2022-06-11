2483ba7f02551fdd7184affabecf8936ab3adbb3
Danya <theincredibledanzel@yandex.ru>
2022-06-11 09:03:43 +0400

    Task_6_complete

--- Task_6_even/Program.cs
+++ Task_6_even/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to the program!");
+Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int remnant = number % 2;
+if (remnant == 0)
+{
+    Console.Write("Your number is even");
+}
+else
+{
+    Console.Write("Your number is odd (uneven)");
+}
+
+Console.WriteLine("");
+Console.Write("Thank you for participating! Program complete^^");
