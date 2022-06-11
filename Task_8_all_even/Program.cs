c3863cd48d29846c1e13625b8fb94aa02448fa1d
Danya <theincredibledanzel@yandex.ru>
2022-06-11 14:15:48 +0400

    Task_8_complete

--- Task_8_all_even/Program.cs
+++ Task_8_all_even/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to the program!");
+Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 4;
+
+if (number >= 1)
+{
+    if (number == 1)
+    {
+    Console.Write("You input number 1 - in this range of values even numbers are missing");
+    }
+    else
+    {
+        Console.Write("Even numbers in set range: 2");
+        while (index <= number)
+        {
+            Console.Write(", ");
+            Console.Write(index);
+            index = index + 2;
+        }
+    }
+}    
+else
+{
+    Console.Write("Your number is incorrect, please restart program and input correct number");
+}
+
+Console.WriteLine("");
+Console.Write("Thank you for participating! Program complete^^");
