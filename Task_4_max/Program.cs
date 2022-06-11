ced07100e77f18d4d9fda1f884a752d6cb50fe04
Danya <theincredibledanzel@yandex.ru>
2022-06-11 08:42:14 +0400

    Task_4_complete

--- Task_4_max/Program.cs
+++ Task_4_max/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to the program!");
+Console.Write("Input first integer number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third integer number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int accum = 0;
+
+if (number1 > number2)
+{
+    accum = number1;
+}
+else
+{
+    accum = number2;
+}
+
+if (accum > number3)
+{
+    Console.Write("Max number = " + accum);
+}
+else
+{
+    Console.Write("Max number = " + number3);
+}
+
+Console.WriteLine("");
+Console.Write("Thank you for participating! Program complete^^");
